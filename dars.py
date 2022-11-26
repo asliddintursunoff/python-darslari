729a091b026c66ffdd5f50513fa60b894535dbe6
asliddintursunoff <asliddintursunoff@icloud.com>
2022-11-26 08:59:12 +0500

    first_project

---
+++ dars.py
@@ -0,0 +1,14 @@
+# a=int(input("hafta kunini kiriting="))
+# if a==1:
+#   print("Dushanba")   
+# if a==2:
+#     print("sechanba")
+# if a==3:
+#     print("chorshanba")
+
+
+# son1=int(input("1 son kiriting"))
+# son2=int(input("2 son kiriting"))
+# son3=int(input("3 son kiriting"))
+# print((son1+son2+son3)//3)
+
